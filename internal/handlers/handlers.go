461d251c91bd36d6ad871dff1a47d3f0f16c0e43
Danya <daniagoldin@yandex.ru>
2025-07-21 13:42:41 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,50 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HandlerIndex(w http.ResponseWriter, r *http.Request) {
+	cwd, _ := os.Getwd()
+	path := filepath.Join(cwd, "..", "index.html")
+	http.ServeFile(w, r, path)
+
+}
+
+func HandlerUpload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "mistake with getting file", http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "mistake with reading file", http.StatusInternalServerError)
+	}
+
+	datastr := service.Morze(string(data))
+
+	safeTime := time.Now().UTC().Format("20060102_150405")
+	filename := safeTime + filepath.Ext(header.Filename)
+
+	err = os.WriteFile(filename, []byte(datastr), 0755)
+	if err != nil {
+		http.Error(w, "Mistake with writing file", http.StatusInternalServerError)
+
+		return
+	}
+
+	w.Write([]byte(datastr))
+
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Serv   *http.Server
+}
+
+func Serv(log *log.Logger) *Server {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.HandlerIndex)
+	mux.HandleFunc("/upload", handlers.HandlerUpload)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     log,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger: log,
+		Serv:   s,
+	}
+
+}
